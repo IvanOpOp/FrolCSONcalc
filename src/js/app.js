246557fced1d061876ba70749b1cf69d20bdd3b5
IvanOpOp <125567983+IvanOpOp@users.noreply.github.com>
2023-04-01 15:43:30 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+let category = document.getElementById("category");
+let benefit = document.getElementById("benefit");
+category.onclick = function(){
+    benefit.classList.toggle("benefit--active")
+};
+let category2 = document.getElementById("category2");
+let benefit2 = document.getElementById("benefit2");
+category2.onclick = function(){
+    benefit2.classList.toggle("benefit--active")
+};
+let category3 = document.getElementById("category3");
+let benefit3 = document.getElementById("benefit3");
+category3.onclick = function(){
+    benefit3.classList.toggle("benefit--active")
+};
+let category4 = document.getElementById("category4");
+let benefit4 = document.getElementById("benefit4");
+category4.onclick = function(){
+    benefit4.classList.toggle("benefit--active")
+};
+let category5 = document.getElementById("category5");
+let benefit5 = document.getElementById("benefit5");
+category5.onclick = function(){
+    benefit5.classList.toggle("benefit--active")
+};
